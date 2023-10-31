491f1808d8af1f187d60f2293a208d009c09db8f
Kehinde Sawyerr <kensawyerr@gmail.com>
2023-10-31 08:13:08 +0100

    modified:   package-lock.json 	modified:   package.json 	deleted:    src/AddCircle.svg 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/logo.svg 	renamed:    nothing-to-see-here-icon.png -> src/nothing-to-see-icon.png

--- src/App.js
+++ src/App.js
@@ -1,37 +1,89 @@
-// import logo from "./logo.svg";
+// import picture from "./nothing-to-see-icon.png";
 
-// import { Sun1 , Add } from "iconsax-react";
+import { TickCircle } from "iconsax-react";
+import { EmojiHappy } from "iconsax-react";
+
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageOk , setImageOk] = useState(null);
+  const handleImage = (event) => {
+    if (
+      event.target.files[0].type !== "image/jpeg" &&
+      event.target.files[0].type !== "image/jpg" &&
+      event.target.files[0].type !== "image/png"
+    ) {
+      alert("invalid file type");
+      console.log(event.target.files[0]);
+    } else {
+      console.log(event.target.files[0]);
+      setSelectedImage(event.target.files[0]);
+      setImageOk()
+    }
+
+    // if (file.type !=="image/jpeg" || file.type !=="image/jpg"  || file.type !=="image/png" ) {
+    //   alert("invalid file type") }
+
+    //   else{
+    //         alert ("sucess")
+    //   //   <div className="  success flex">
+    //   //   <div className="container flex">
+    //   //   <TickCircle /> Upload successful
+    //   // </div>
+    //   // </div>
+    //   }
+  };
+
   return (
     <div className="App container ">
       <nav className="flex">
-        <ul className="flex margin-right">
-          <li className="logo">PhotoStash </li> <li>Gallery</li>
-        </ul>
-        <ul className="flex">
-          <li>Sign in</li>
-        </ul>
+        <div className="container flex">
+          <ul className="flex margin-right">
+            <li className="logo">PhotoStash</li> <li>Gallery</li>
+          </ul>
+          <ul className="flex">
+            <li>Sign in</li>
+          </ul>
+        </div>
       </nav>
+      {imageOk && ( 
+      <div className="  success flex">
+        <div className="container flex">
+          <TickCircle /> Upload successful
+        </div>
+      </div> )}
       <main>
         <section>
-          <label for="input-file">
-            <div className="box"></div>
-          </label>
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <div className="box grid">
+            <label htmlFor="input-file">
+              {selectedImage && (
+                <img
+                  src={URL.createObjectURL(selectedImage)}
+                  alt="add button"
+                  className="input-img"
+                />
+              )}
+            </label>
+            <EmojiHappy />
+            <p className="justify-f-strt">Drag and drop here ;)</p>
+          </div>
 
           <input
             type="file"
             accept="image/png, image/jpeg, image/jpg"
             id="input-file"
             className="hide"
+            onChange={handleImage}
           />
 
-          <label for="input-file" className="btn">
+          <label htmlFor="input-file" className="btn">
             Upload a photo
           </label>
-             <a  href="#" className="terms"><p>only jpeg, png and jpg files</p></a>
+          <a href="/src/gallery" className="terms">
+            <p>only jpeg, png and jpg files</p>
+          </a>
           <a
             className="App-link"
             href="https://reactjs.org"
